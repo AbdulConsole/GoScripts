f8a0257cafbc8d90b55a7dee1ad17cd95679ac9b
AbdulConsole <hzwedt@gmail.com>
2021-10-10 14:33:53 +0100

    htmltise struct content

---
+++ gohtml template5/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type Person struct {
+	Name    string
+	Grade   string
+	Pronoun string
+}
+
+func main() {
+
+	roll1 := Person{
+		Name:    "Abdul Console",
+		Grade:   "A1",
+		Pronoun: "his",
+	}
+	roll2 := Person{
+		Name:    "John Mary",
+		Grade:   "B2",
+		Pronoun: "her",
+	}
+	roll3 := Person{
+		Name:    "James Tony",
+		Grade:   "C4",
+		Pronoun: "his",
+	}
+	roll4 := Person{
+		Name:    "Ibrahim Okey",
+		Grade:   "A+",
+		Pronoun: "his",
+	}
+	roll5 := Person{
+		Name:    "Ali Aduke",
+		Grade:   "B+",
+		Pronoun: "her",
+	}
+
+	persons := []Person{roll1, roll2, roll3, roll4, roll5}
+	template1, err := template.ParseFiles("template.gohtml")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	template1.ExecuteTemplate(os.Stdout, "template.gohtml", persons)
+}
